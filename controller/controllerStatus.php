ad80347398a95058507b3d775a33b9c013be82b2
Gabriela Paulino <gabrielappsilva18@gmail.com>
2024-07-05 19:54:35 -0300

    Atualizado

--- controller/controllerStatus.php
+++ controller/controllerStatus.php
@@ -1,33 +1,29 @@
 <?php
-
-class controllerStatus {
-
-    public function listarStatus() {
-        try {
-            $modelStatus = new modelStatus();
-            return $modelStatus->listarStatus();
-        } catch (PDOException $e) {
-            return false;
-        }
-    }
-
-    public function cadastrarStatus($nome_status) {
-        try {
-            $modelStatus = new modelStatus();
-            return $modelStatus->cadastrarStatus($nome_status);
-        } catch (PDOException $e) {
-            return false;
-        }
-    }
-
-    public function atualizarStatus($nome_status, $id_status){
-        try{
-            $modelStatus = new modelStatus();
-            return $modelStatus->atualizarStatus($nome_status, $id_status);
-
-        } catch(PDOException $e){
-            return false;
-        }
-    }
-
+class controllerStatus{
+   public function listarStatus(){
+       try{
+           $modelStatus = new modelStatus();
+           return $modelStatus->listarStatus();
+       }catch(PDOException){
+           return false;
+       }
+   }
+   public function cadastrarStatus($descricao){
+       try{
+           $modelStatus = new modelStatus();
+           return $modelStatus->cadastrarStatus($descricao);
+       }catch(PDOException $e){
+           return false;
+       }
+   }
+   public function excluirStatus($id_status){
+       try{
+           $modelStatus = new modelStatus();
+           return $modelStatus->excluirStatus($id_status);
+       }catch(PDOException $e){
+           return false;
+       }
+   }
 }
+
+?>
--- model/modelStatus.php
+++ model/modelStatus.php
@@ -1,46 +1,38 @@
 <?php
-
-class modelStatus {
-
-    public function listarStatus() {
-        try {
-            $pdo = Database::conexao();
-            $consulta = $pdo->query("SELECT * FROM tbl_produtos");
-            $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
-
-            return $resultado;
-        } catch(PDOException $e) {
-            return false;
-        }
-    }
-
-    public function cadastrarStatus() {
-        try {
-            $pdo = Database::conexao();
-            $inserir = $pdo->prepare("INSERT INTO tbl_status (nome_status, data_cadastro)
-                                      VALUES (:nome_status, now() )");
-            $inserir->bindParam(":nome_status", $nome_status);
-            $inserir->bindParam(":data_cadastro", $data_cadastro);
-            $inserir->execute();
-
-            return true;
-        } catch (PDOException $e) {
-            return false;
-        }
-    }
-
-    public function atualizarStatus($nome_status, $id_status){
-        try{ 
-         $pdo = Database::conexao();
-         $atualizar = $pdo->prepare("UPDATE tbl_status SET nome_status = :nome_status, data_cadastro = now() WHERE id_status = :id_status");
-         $atualizar->bindParam(":nome_status", $nome_status);
-         $atualizar->bindParam(":id_status", $id_status);
-         $atualizar->execute();
-         return true;
- 
-        }catch(PDOException $e){
-         return false;
-        }
-     
-     }
+class modelStatus{
+   public function listarStatus(){
+       try{
+           $pdo = Database::conexao();
+           $sql = $pdo->query("SELECT * FROM  tbl_status");
+           $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
+           return $resultado;  
+       }catch(PDOException $e){
+           return false;
+       }
+   }
+   public function cadastrarStatus($descricao){
+       try{
+           $pdo= Database::conexao();
+           $descricao_filtro = htmlspecialchars($descricao, ENT_QUOTES);
+           $prepare = $pdo->prepare("INSERT INTO tbl_status (descricao)  VALUES (:descricao)");
+           $prepare->bindParam("descricao",$descricao_filtro );
+           $prepare->execute();
+           return true;
+       }catch(PDOException $e){
+           return false;
+       }
+   }
+   public function excluirStatus($id_status){
+       try{
+           $pdo= Database::conexao();
+           $prepare = $pdo->prepare("DELETE FROM tbl_status WHERE id_status = :id_status");
+           $prepare->bindParam("id_status",$id_status);
+           $prepare->execute();
+           return true;
+       }catch(PDOException $e){
+           return false;
+       }
+   }
 }
+
+?>
